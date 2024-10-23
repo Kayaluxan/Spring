33c20019b8bb414c8177f4e35a2144171ca0160c
piragenthtech <piragenthtech@gmail.com>
2024-10-23 12:37:57 +0530

    Helloo

---
+++ Employee/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,61 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	@Id
+	private int id;
+	 @Column(name = "first_name")
+	 private String firstName;
+	 @Column(name = "last_name")
+	 private String lastName;
+	 @Column(name = "email_id")
+	 private String emailId;
+	public Employee(int id, String firstName, String lastName, String emailId) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+	public Employee() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId
+				+ "]";
+	}
+	 
+	 
+
+}
